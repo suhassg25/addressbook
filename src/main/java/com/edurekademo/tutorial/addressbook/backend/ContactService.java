bbcdb4d5904a843d154555b0d706ed2014a4428f
suhas <suhas0205@gmail.com>
2026-04-13 11:21:25 +0530

    backend changes

--- src/main/java/com/edurekademo/tutorial/addressbook/backend/ContactService.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/backend/ContactService.java
@@ -17,13 +17,13 @@ public class ContactService {
 
     // Create dummy data by randomly combining first and last names
     static String[] fnames = { "Peter", "Alice", "John", "Mike", "Olivia",
-            "Nina", "Alex", "Rita", "Dan", "Umberto", "Henrik", "Rene", "Lisa",
+            "Nina", "Alex", "Riya", "Dan", "Umberto", "Henrik", "Rene", "Lisa",
             "Linda", "Timothy", "Daniel", "Brian", "George", "Scott",
-            "Jennifer" };
+            "Jennifer, Lingaraj" };
     static String[] lnames = { "Smith", "Johnson", "Williams", "Jones",
             "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor",
             "Anderson", "Thomas", "Jackson", "White", "Harris", "Martin",
-            "Thompson", "Young", "King", "Robinson" };
+            "Thompson", "Young", "King", "Robinson", hasan };
 
     private static ContactService instance;
 
